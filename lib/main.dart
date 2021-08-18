0216291ac77c07f1eefc6a5031f5e6345d132461
sumankhatri404 <sumitkhatri858.com>
2021-08-18 19:17:58 +0545

    todo app with firestore database crud

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_app/pages/home_page.dart';
+
+import 'package:todo_app/provider/todos.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static final title = "Todo App";
+
+  @override
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+        create: (context) => TodosProvider(),
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: title,
+          theme: ThemeData(
+            primarySwatch: Colors.teal,
+            scaffoldBackgroundColor: Color(0xFFf6f5ee),
+          ),
+          home: HomePage(),
+        ),
+      );
+}
